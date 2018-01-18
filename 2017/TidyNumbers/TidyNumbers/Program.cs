9e1ce825276df85575bae728d3486ad9e34a8b83
Peter Garzoni <peter.garzoni@gmail.com>
2018-01-17 20:50:49 -0500

    Tidy Numbers

---
+++ 2017/TidyNumbers/TidyNumbers/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TidyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TidyNumbersSimulation(Path.GetFullPath(@"..\..\B-small-practice.in"), Path.GetFullPath(@"..\..\outSmall.txt"));
+
+            TidyNumbersSimulation(Path.GetFullPath(@"..\..\B-large-practice.in"), Path.GetFullPath(@"..\..\outLarge.txt"));
+        }
+
+        public static void TidyNumbersSimulation(string inFile, string outFile)
+        {
+            StreamReader sr = new StreamReader(inFile);
+            StreamWriter sw = new StreamWriter(outFile);
+            string caseStr = sr.ReadLine();
+            int totalCases = Int32.Parse(caseStr);
+
+            for (int i = 1; i <= totalCases; i++)
+            {
+                caseStr = sr.ReadLine();
+                long tidyNum = GetHighestTidyNumber(long.Parse(caseStr));
+                sw.WriteLine("Case #{0}: {1}", i, tidyNum);
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+
+        public static bool IsTidyNumber(long value)
+        {
+            string number = value.ToString();
+            bool flag = false;
+
+            if (number.Length == 1)
+                flag = true;
+            else
+            {
+                for (int i = 0; i < number.Length; i++)
+                {
+                    if (i + 1 < number.Length)
+                    {
+                        int a = number.ElementAt(i);
+                        int b = number.ElementAt(i + 1);
+
+                        if (a <= b)
+                            flag = true;
+                        else
+                            return false;
+                    }
+                }
+            }
+            return flag;
+        }
+
+        public static long GetHighestTidyNumber(long value)
+        {
+            long number = value;
+            long mod = 10;
+
+            while (!IsTidyNumber(number))
+            {
+                if (number % 10 == 0)
+                    number--;
+                else
+                {
+                    number -= (number % mod) + 1;
+                    mod *= 10;
+                }
+            }
+            return number;
+        }
+    }
+}
